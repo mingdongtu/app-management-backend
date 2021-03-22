d41fce722707c8f1c6d8f6e612ef1c4c49d24dea
mingdong <tumingdong@sdbattery.com>
2021-03-22 16:54:48 +0800

    增加热更新配置已经优化sql查询

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,16 +5,24 @@ const tools = require('./../utils/tool')
 module.exports = {
       async getLogin(ctx){
         const data = ctx.request.url;
-       
          const params = tools.parseUrl(data)
-           let result =await infoService.getUserData(data)
-           console.log(tools.parseUrl(data),result[0])
-          
-           ctx.body ={
-                code:0,
-                msg:'登录成功',
-                data:result
+           let result =await infoService.getUserData(params)
+           if(result.length<1){
+                //  没有这个用户
+                ctx.body ={
+                  code:0,
+                  msg:'不存在这个用户',
+                  data:null
+             }
+
+           }else{
+            ctx.body ={
+              code:1,
+              msg:'登录成功',
+              data:result
+         }
            }
+           
           //  ctx.body =result  
            
       }
--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,9 @@ const dbUtils = require('./../utils/db')
 const info = {
      async getData(data){
         console.log('调用models层',data)
-          let sql = 'select * from userInfo';
+        const {username,password} = data
+          let sql = `SELECT * FROM userInfo WHERE username ="${username}"  AND password = "${password}"`;
+           console.log("我的查询语句",sql)
           let  result = await dbUtils.query(sql)
           return result
      }
